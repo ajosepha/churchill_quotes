a5c5373d6c2880259e93918e714bb6756956852f
ajosepha <ariel.isaacs@gmail.com>
2013-12-10 23:04:09 -0500

    delete useless files and add scraper with quotes array

--- lib/churchill_scraper.rb
+++ lib/churchill_scraper.rb
@@ -7,18 +7,19 @@ class Scraper
 
     attr_reader :html
 
-    def initialize(url)
-        
+    def initialize(url)   
         download = open(url)
         @html = Nokogiri::HTML(download)
     end    
 
     def get_quote
+        #this returns an array
         quote = html.search("p")
         quote1 = quote.map {|name| name.text}
         quote1.map! do |string|
             string.delete "\r\n"
         end
+        quote1.delete_if{|x| x == "" }
         ap quote1
     end    
 end  
