b6ec7c7413357c315fad26586fdc28f8f7cf0506
ajosepha <ariel.isaacs@gmail.com>
2013-12-12 13:38:44 -0500

    add with poor formatting

--- app.rb
+++ app.rb
@@ -9,11 +9,13 @@ module Churchill_Generator
     before do
         @quotes = Scraper.new("http://jpetrie.myweb.uga.edu/bulldog.html")
         @quote_array = @quotes.get_quote 
+        @images = Dir["./public/images/*"].map {|image| "/images/#{Pathname.new(image).basename}"}
     end 
     
     get '/home' do
-      "here are some quotes"
+      "here are some quotes and pictures"
       @quote = @quote_array.sample
+      @image = @images.sample
 
       erb :quotes
     end
